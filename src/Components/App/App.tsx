b72bb53999b0f93bb12227f2e338299faf508427
Lredigonda <leoredigonda@hotmail.com>
2022-08-31 20:36:28 -0300

    initial Commit

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola a todos este es mi pito de goma</h1>
+
+      
+    </div>
+  );
+}
+
+export default App;
